7a508975d34503906880597746f749a6dcfd8405
Vava Heirro <vavaheirro@live.com>
2020-07-20 14:00:45 +0700

    Update DownloadItem.js

--- web/src/components/DownloadItem.js
+++ web/src/components/DownloadItem.js
@@ -51,7 +51,7 @@ function DownloadItem({ torrent }) {
           </button>
         )}
         {torrent.done && (
-          <a href={'https://sgb:sgbteam@sgbteam.zeth.workers.dev/'} target="_blank" className="btn success">
+          <a href={'https://files.zeth.workers.dev/Leech/'} target="_blank" className="btn success">
             Open
           </a>
         )}
